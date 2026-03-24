7ca16fbddcc96d39c02214ed709c1b448a0a0217
Siddharth Joshi <59554898+Siddharth854@users.noreply.github.com>
2026-03-24 11:43:26 +0530

    Time: 0 ms (100%), Space: 43.8 MB (55.2%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int last = m+n-1;
-        //merge in reverse order
+        //merge in reverse order 
         while(m>0 && n>0)
         {
             if(nums1[m-1] > nums2[n-1])
