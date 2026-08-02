297380e41152f52069d4128111bbfc54580cc9c8
Siddharth Joshi <59554898+Siddharth854@users.noreply.github.com>
2026-08-02 12:23:08 +0530

    Time: 1 ms (99.95%), Space: 78.7 MB (13.3%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int[] prefix = new int[n];
         prefix[0] = nums[0];
-        int ans = nums[0];
+        int ans = prefix[0];
         for(int i=1;i<n;i++)
         {
             prefix[i] = Math.max(prefix[i-1]+nums[i],nums[i]);
