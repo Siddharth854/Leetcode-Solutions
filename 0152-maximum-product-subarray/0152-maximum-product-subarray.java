509232481f76c00aad6e6f96754b1d9586435991
Siddharth Joshi <59554898+Siddharth854@users.noreply.github.com>
2026-08-08 00:00:58 +0530

    Time: 2 ms (71.74%), Space: 47.1 MB (73.85%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -4,11 +4,11 @@ class Solution {
         int max = nums[0];
         int min = nums[0];
         int ans = nums[0];
-        for(int i=1; i<n; i++)
+        for(int i=1;i<n;i++)
         {
             int temp = max;
-            max = Math.max(nums[i],Math.max(max*nums[i],min*nums[i]));
-            min = Math.min(nums[i],Math.min(temp*nums[i],min*nums[i]));
+            max = Math.max(nums[i],Math.max(max * nums[i],min*nums[i]));
+            min = Math.min(nums[i],Math.min(temp * nums[i],min*nums[i]));
             ans = Math.max(ans,max);
         }
         return ans;
